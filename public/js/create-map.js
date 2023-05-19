bd4e4af8308f1b34b8c5155e261db508e4fca934
IreneValor <115938535+IreneValor@users.noreply.github.com>
2023-05-20 01:15:51 +0200

    intento fallido paginación. Reduzco los resultados para que no se bloquee

--- public/js/create-map.js
+++ public/js/create-map.js
@@ -1,14 +1,18 @@
 const MAPBOX_ACCESS_TOKEN =
   "pk.eyJ1IjoiaXJlbmVndiIsImEiOiJjbGhyd2xza3EyZm1zM2xvZGNnNjI3NXJxIn0.ENRxlLmYlxI7a7vJKsHbsQ";
 
-const main = () => {
-  
-  mapboxgl.accessToken = MAPBOX_ACCESS_TOKEN;
-  const mapas = document.querySelectorAll('[id^="map-"]'); 
-  const canvas = document.getElementById("myCanvas");
+// Numero de mapas por pagina
+const MAPS_PAGE = 5;
+// Pagina actual
+let page = 0;
 
-  mapas.forEach((mapa) => {
-    const coords = mapa?.id?.replace("map-", "").split("/");
+const main = () => {
+  mapboxgl.accessToken = MAPBOX_ACCESS_TOKEN;
+  const mapas = document.querySelectorAll('[id^="map-"]');
+  const initialMaps = Object.entries(mapas).slice(page * MAPS_PAGE, MAPS_PAGE);
+
+  initialMaps.forEach((mapa) => {
+    const coords = mapa[1]?.id?.replace("map-", "").split("/");
     const latitude = parseFloat(coords[0]);
     const longitude = parseFloat(coords[1]);
 
@@ -18,7 +22,7 @@ const main = () => {
     }
 
     const map = new mapboxgl.Map({
-      container: mapa?.id,
+      container: mapa[1]?.id,
       zoom: 15,
       center: [longitude, latitude],
       style: "mapbox://styles/mapbox/streets-v11",
@@ -30,3 +34,28 @@ const main = () => {
 
 window.addEventListener("load", main);
 
+const loadNextResults = () => {
+  const mapas = document.querySelectorAll('[id^="map-"]');
+  const nextResults = Object.entries(mapas).slice(page * MAPS_PAGE, MAPS_PAGE);
+
+  console.log(nextResults);
+  nextResults.forEach((mapa) => {
+    const coords = mapa[1]?.id?.replace("map-", "").split("/");
+    const latitude = parseFloat(coords[0]);
+    const longitude = parseFloat(coords[1]);
+
+    if (isNaN(longitude) || isNaN(latitude)) {
+      console.error("Error: Las coordenadas deben ser números");
+      return;
+    }
+
+    const map = new mapboxgl.Map({
+      container: mapa[1]?.id,
+      zoom: 15,
+      center: [longitude, latitude],
+      style: "mapbox://styles/mapbox/streets-v11",
+    });
+
+    new mapboxgl.Marker().setLngLat([longitude, latitude]).addTo(map);
+  });
+};
--- routes/findPlaces.routes.js
+++ routes/findPlaces.routes.js
@@ -6,10 +6,11 @@ const { isLoggedIn } = require("../middlewares/route-guard");
 const uploader = require("../config/claudinary.config");
 const axios = require("axios");
 const hbs = require("hbs");
-//const handlebarsHelpers = require("handlebars-helpers");
+// const handlebarsHelpers = require("handlebars-helpers");
 
-// Registra todos los helpers proporcionados por el paquete handlebars-helpers
-//hbs.registerHelpers(handlebarsHelpers);
+// hbs.registerHelpers(handlebarsHelpers);
+
+const RESULTS_PER_PAGE = 10; // Cantidad de resultados por página
 
 router.get("/search-places", isLoggedIn, async (req, res, next) => {
   res.render("experienceSearch", { user: req.session.currentUser });
@@ -17,14 +18,11 @@ router.get("/search-places", isLoggedIn, async (req, res, next) => {
 
 router.post("/search-places", isLoggedIn, async (req, res, next) => {
   let { location, category, name } = req.body;
-  // llamamos a la API
-  // http://tour-pedia.org/api/getPlaces?category=restaurant&location=Berlin&name=La+Dolce+Vita
-  // bar california
   if (name) {
-    let words = name.split(" "); // split the string into an array of words
+    let words = name.split(" ");
     for (let i = 0; i < words.length; i++) {
       let word = words[i];
-      words[i] = word.charAt(0).toUpperCase() + word.slice(1); // capitalize the first letter of each word
+      words[i] = word.charAt(0).toUpperCase() + word.slice(1);
     }
     name = words.join("+");
   }
@@ -33,12 +31,19 @@ router.post("/search-places", isLoggedIn, async (req, res, next) => {
   axios
     .get(urlAPi)
     .then((response) => {
+      const totalResults = response.data.length;
+      const totalPages = Math.ceil(totalResults / RESULTS_PER_PAGE);
+      const currentPage = 1;
+      const startIndex = (currentPage - 1) * RESULTS_PER_PAGE;
+      const endIndex = startIndex + RESULTS_PER_PAGE;
+      const paginatedResults = response.data.slice(startIndex, endIndex);
+
       res.render("search-places-results", {
-        response: response.data,
+        response: paginatedResults,
         user: req.session.currentUser,
+        totalPages: totalPages,
+        currentPage: currentPage,
       });
-      console.log(response.data);
-      //los datos de l y l para mapa estan en response.data y se envian bien
     })
     .catch((error) => {
       console.log("Tu error es", error);
@@ -46,61 +51,47 @@ router.post("/search-places", isLoggedIn, async (req, res, next) => {
 });
 
 module.exports = router;
-//dime como puedo usa esta ultima parte del codigo get para usar paginacion
+// const router = require("express").Router();
+// const Experience = require("../models/Experience.model");
+// const User = require("../models/User.model");
+// const Review = require("../models/Review.model");
+// const { isLoggedIn } = require("../middlewares/route-guard");
+// const uploader = require("../config/claudinary.config");
+// const axios = require("axios");
+// const hbs = require("hbs");
+// const handlebarsHelpers = require("handlebars-helpers");
+// // Registra todos los helpers proporcionados por el paquete handlebars-helpers
+// hbs.registerHelpers(handlebarsHelpers);
 
-//----paginacion
-// axios
+// router.get("/search-places", isLoggedIn, async (req, res, next) => {
+//   res.render("experienceSearch", { user: req.session.currentUser });
+// });
+
+// router.post("/search-places", isLoggedIn, async (req, res, next) => {
+//   let { location, category, name } = req.body;
+//   if (name) {
+//     let words = name.split(" "); // split the string into an array of words
+//     for (let i = 0; i < words.length; i++) {
+//       let word = words[i];
+//       words[i] = word.charAt(0).toUpperCase() + word.slice(1); // capitalize the first letter of each word
+//     }
+//     name = words.join("+");
+//   }
+//   let urlAPi = `http://tour-pedia.org/api/getPlaces?category=${category}&location=${location}&name=${name}`;
+
+//   axios
 //     .get(urlAPi)
 //     .then((response) => {
-//       const pageSize = 10; // Número de resultados por página
-//       const totalResults = response.data.length;
-//       const totalPages = Math.ceil(totalResults / pageSize);
-//       const startIndex = (page - 1) * pageSize;
-//       const endIndex = startIndex + pageSize;
-//       const paginatedResults = response.data.slice(startIndex, endIndex);
-
 //       res.render("search-places-results", {
-//         response: paginatedResults,
+//         response: response.data,
 //         user: req.session.currentUser,
-//         currentPage: page,
-//         totalPages: totalPages,
 //       });
-//       console.log(paginatedResults);
-//       // Los datos de l y l para mapa están en response.data y se envían correctamente
+//       console.log(response.data);
+//       //los datos de l y l para mapa estan en response.data y se envian bien
 //     })
 //     .catch((error) => {
 //       console.log("Tu error es", error);
 //     });
-//     });
-
-//DETALLES API, ME DEVUELVE LOS DATOS PERO NO ME APARECE EL MAPA, AUNQUE LG Y LONG ME LOS DA
-// router.get("/search-places/details/:id", isLoggedIn, async (req, res, next) => {
-//   try {
-//     const experienceId = req.params.id;
-//     const urlApi = `http://tour-pedia.org/api/getPlaceDetails?id=${experienceId}`;
-//     const response = await axios.get(urlApi);
-//     const experienceData = response.data;
-
-//     if (!experienceData) {
-//       // Lógica para manejar el caso en el que no se encuentren datos de la experiencia
-//       // Por ejemplo, mostrar un mensaje de error o redirigir a otra página
-//     } else {
-//       const coordinates = experienceData.map((place) => [place.lng, place.lat]);
-//       const map = createMap(coordinates);
-
-//       const detailsData = experienceData.find(
-//         (item) => item.id === parseInt(experienceId)
-//       );
-
-//       res.render("details-experience", {
-//         details: detailsData,
-//         map: map, // Pasa el mapa como variable a la vista
-//       });
-//     }
-//   } catch (error) {
-//     console.log(error);
-//     // Lógica para manejar el error en caso de que ocurra un problema al llamar a la API
-//   }
 // });
 
 // module.exports = router;
